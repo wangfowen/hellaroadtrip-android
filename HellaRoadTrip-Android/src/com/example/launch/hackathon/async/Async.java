88264394609bb95a478cfc43e669460da3b6ab64
nnarayanan <nnarayanan@appdynamics.com>
2013-03-02 23:43:13 -0800

    Async Task class

---
+++ HellaRoadTrip-Android/src/com/example/launch/hackathon/async/Async.java
@@ -0,0 +1,25 @@
+package com.example.launch.hackathon.async;
+
+import android.os.AsyncTask;
+
+public class Async extends AsyncTask<String, Void, Void> {
+
+	@Override
+	protected Void doInBackground(String... args) {
+		
+		return null;
+	}
+	
+	@Override
+    protected void onPreExecute() 
+    {
+             
+    }
+
+    @Override
+    protected void onPostExecute(Void result) 
+    {
+
+    }   
+
+}

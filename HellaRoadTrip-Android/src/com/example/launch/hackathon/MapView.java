19224ce3202a13aa4b15d521a1863c499136217f
nnarayanan <nnarayanan@appdynamics.com>
2013-03-03 00:30:57 -0800

    Added create dialog function

--- HellaRoadTrip-Android/src/com/example/launch/hackathon/MapView.java
+++ HellaRoadTrip-Android/src/com/example/launch/hackathon/MapView.java
@@ -7,9 +7,12 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -127,7 +130,8 @@ public class MapView extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch(item.getItemId()) {
     		case R.id.action_add:
-    			Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+    			//Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+    			createAddMarkerDialog().show();
     			return true;
     		default:
     			Toast.makeText(this, "Selected Item: none", Toast.LENGTH_SHORT).show();
@@ -168,6 +172,29 @@ public class MapView extends Activity {
 		else {
 			gps.showSettingsAlert();
 		}
+    }
+    
+    public AlertDialog createAddMarkerDialog() {
+    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Get the layout inflater
+        LayoutInflater inflater = getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_add, null))
+        // Add action buttons
+               .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int id) {
+                       // sign in the user ...
+                   }
+               })
+               .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       dialog.cancel();
+                   }
+               });      
+        return builder.create();
     }
     
     /*private void getSortedLocations(HashMap<Integer, Double> distance, ArrayList<HashMap<String, String>> data) {

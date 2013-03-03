de91ccbc88cca8914e441e08f4057f9d1f2717b6
nnarayanan <nnarayanan@appdynamics.com>
2013-03-03 03:48:18 -0800

    Updated logo and mved google_play_services

--- HellaRoadTrip-Android/src/com/example/launch/hackathon/MapView.java
+++ HellaRoadTrip-Android/src/com/example/launch/hackathon/MapView.java
@@ -1,8 +1,18 @@
 package com.example.launch.hackathon;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -11,6 +21,7 @@ import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,13 +29,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.launch.hackathon.async.Async;
 import com.example.launch.hackathon.common.GPSTracker;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -130,6 +145,7 @@ public class MapView extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch(item.getItemId()) {
     		case R.id.action_add:
+    			new Async().execute(new String[] {""});
     			//Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
     			createAddMarkerDialog().show();
     			return true;
@@ -178,22 +194,49 @@ public class MapView extends Activity {
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
         // Get the layout inflater
         LayoutInflater inflater = getLayoutInflater();
-
+        
+        View dialogView = inflater.inflate(R.layout.dialog_add, null);
+        Spinner spinner = (Spinner) dialogView.findViewById(R.id.drivers);
+	     // Create an ArrayAdapter using the string array and a default spinner layout
+	     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+	             R.array.marker_types, android.R.layout.simple_spinner_item);
+	     // Specify the layout to use when the list of choices appears
+	     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	     // Apply the adapter to the spinner
+	     spinner.setAdapter(adapter);
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(inflater.inflate(R.layout.dialog_add, null))
+        builder.setView(dialogView)
         // Add action buttons
                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                    @Override
                    public void onClick(DialogInterface dialog, int id) {
-                       // sign in the user ...
+                	   
+                	   
+                	   
+                	   
+                       myMap.setOnMapClickListener(new OnMapClickListener() {
+						
+						@Override
+						public void onMapClick(LatLng point) {
+							i++;
+							message = "Point: "+ i + " at "+ point.toString();
+							Marker marker = myMap.addMarker(new MarkerOptions().position(point).title(point.toString()));
+							myMap.addPolyline(new PolylineOptions()
+						       .add(new LatLng(gps.getLatitude(), gps.getLongitude()), marker.getPosition())
+						       .width(5)
+						       .color(Color.BLACK));
+							
+						}
+					});
                    }
                })
                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
                        dialog.cancel();
                    }
-               });      
+               });  
+        
         return builder.create();
     }
     
--- HellaRoadTrip-Android/src/com/example/launch/hackathon/async/Async.java
+++ HellaRoadTrip-Android/src/com/example/launch/hackathon/async/Async.java
@@ -1,12 +1,46 @@
 package com.example.launch.hackathon.async;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import android.os.AsyncTask;
 
 public class Async extends AsyncTask<String, Void, Void> {
 
 	@Override
 	protected Void doInBackground(String... args) {
-		
+		// Create a new HttpClient and Post Header
+	    HttpClient httpclient = new DefaultHttpClient();
+	    HttpPost httppost = new HttpPost("http://hellaroadtrip.herokuapp.com/routing");
+
+	    try {
+	        // Add your data
+	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+	        nameValuePairs.add(new BasicNameValuePair("data", "hi"));
+	        //nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+	        // Execute HTTP Post Request
+	        HttpResponse response = httpclient.execute(httppost);
+	        String responses = EntityUtils.toString(response.getEntity());
+	        System.out.println(responses);
+	        
+	    } catch (ClientProtocolException e) {
+	        // TODO Auto-generated catch block
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	    }
 		return null;
 	}
 	
@@ -19,7 +53,8 @@ public class Async extends AsyncTask<String, Void, Void> {
     @Override
     protected void onPostExecute(Void result) 
     {
-
+    	System.out.println("Hello");
+    	System.out.println(result.toString());
     }   
 
 }
